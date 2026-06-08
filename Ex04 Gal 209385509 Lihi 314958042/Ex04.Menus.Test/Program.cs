045764bdbd95b3143991ef7acb22c3d91ef4afd5
lihicohen122 <lihicohen122@gmail.com>
2026-06-08 15:16:25 +0300

    - divided long methods in Program.

--- Ex04 Gal 209385509 Lihi 314958042/Ex04.Menus.Test/Program.cs
+++ Ex04 Gal 209385509 Lihi 314958042/Ex04.Menus.Test/Program.cs
@@ -13,47 +13,79 @@ namespace Ex04.Menus.Test
         private static void runInterfaceMenuDemo()
         {
             Interfaces.MainMenu interfaceMenu = new Interfaces.MainMenu("Interfaces Main Menu");
-            Interfaces.MenuItem dateTimeSubMenu = new Interfaces.MenuItem("Show Current Date/Time");
-            Interfaces.MenuItem dateItem = new Interfaces.MenuItem("Show Current Date");
-            Interfaces.MenuItem timeItem = new Interfaces.MenuItem("Show Current Time");
-            Interfaces.MenuItem versionSubMenu = new Interfaces.MenuItem("Version and Capitals");
-            Interfaces.MenuItem capitalsItem = new Interfaces.MenuItem("Count Capitals");
-            Interfaces.MenuItem versionItem = new Interfaces.MenuItem("Show Version");
+            Interfaces.MenuItem dateTimeSubMenu = createInterfaceDateTimeSubMenu();
+            Interfaces.MenuItem versionSubMenu = createInterfaceVersionSubMenu();
 
-            dateItem.AttachListener(new DateShowAction());
-            timeItem.AttachListener(new TimeShowAction());
-            capitalsItem.AttachListener(new CapitalsCountAction());
-            versionItem.AttachListener(new VersionShowAction());
-            dateTimeSubMenu.AddSubItem(dateItem);
-            dateTimeSubMenu.AddSubItem(timeItem);
-            versionSubMenu.AddSubItem(capitalsItem);
-            versionSubMenu.AddSubItem(versionItem);
             interfaceMenu.RootMenuItem.AddSubItem(dateTimeSubMenu);
             interfaceMenu.RootMenuItem.AddSubItem(versionSubMenu);
             interfaceMenu.Show();
         }
 
+        private static Interfaces.MenuItem createInterfaceDateTimeSubMenu()
+        {
+            Interfaces.MenuItem dateTimeSubMenu = new Interfaces.MenuItem("Show Current Date/Time");
+            Interfaces.MenuItem dateItem = new Interfaces.MenuItem("Show Current Date");
+            Interfaces.MenuItem timeItem = new Interfaces.MenuItem("Show Current Time");
+
+            dateItem.AttachListener(new DateShowAction());
+            timeItem.AttachListener(new TimeShowAction());
+            dateTimeSubMenu.AddSubItem(dateItem);
+            dateTimeSubMenu.AddSubItem(timeItem);
+
+            return dateTimeSubMenu;
+        }
+
+        private static Interfaces.MenuItem createInterfaceVersionSubMenu()
+        {
+            Interfaces.MenuItem versionSubMenu = new Interfaces.MenuItem("Version and Capitals");
+            Interfaces.MenuItem capitalsItem = new Interfaces.MenuItem("Count Capitals");
+            Interfaces.MenuItem versionItem = new Interfaces.MenuItem("Show Version");
+
+            capitalsItem.AttachListener(new CapitalsCountAction());
+            versionItem.AttachListener(new VersionShowAction());
+            versionSubMenu.AddSubItem(capitalsItem);
+            versionSubMenu.AddSubItem(versionItem);
+
+            return versionSubMenu;
+        }
+
         private static void runEventsMenuDemo()
         {
             Events.MainMenu eventsMenu = new Events.MainMenu("Delegates Main Menu");
+            Events.MenuItem dateTimeSubMenu = createEventsDateTimeSubMenu();
+            Events.MenuItem versionSubMenu = createEventsVersionSubMenu();
+
+            eventsMenu.RootMenuItem.AddSubItem(dateTimeSubMenu);
+            eventsMenu.RootMenuItem.AddSubItem(versionSubMenu);
+            eventsMenu.Show();
+        }
+
+        private static Events.MenuItem createEventsDateTimeSubMenu()
+        {
             Events.MenuItem dateTimeSubMenu = new Events.MenuItem("Show Current Date/Time");
             Events.MenuItem dateItem = new Events.MenuItem("Show Current Date");
             Events.MenuItem timeItem = new Events.MenuItem("Show Current Time");
+
+            dateItem.Selected += showCurrentDate;
+            timeItem.Selected += showCurrentTime;
+            dateTimeSubMenu.AddSubItem(dateItem);
+            dateTimeSubMenu.AddSubItem(timeItem);
+
+            return dateTimeSubMenu;
+        }
+
+        private static Events.MenuItem createEventsVersionSubMenu()
+        {
             Events.MenuItem versionSubMenu = new Events.MenuItem("Version and Capitals");
             Events.MenuItem capitalsItem = new Events.MenuItem("Count Capitals");
             Events.MenuItem versionItem = new Events.MenuItem("Show Version");
 
-            dateItem.Selected += showCurrentDate;
-            timeItem.Selected += showCurrentTime;
             capitalsItem.Selected += countCapitals;
             versionItem.Selected += showVersion;
-            dateTimeSubMenu.AddSubItem(dateItem);
-            dateTimeSubMenu.AddSubItem(timeItem);
             versionSubMenu.AddSubItem(capitalsItem);
             versionSubMenu.AddSubItem(versionItem);
-            eventsMenu.RootMenuItem.AddSubItem(dateTimeSubMenu);
-            eventsMenu.RootMenuItem.AddSubItem(versionSubMenu);
-            eventsMenu.Show();
+
+            return versionSubMenu;
         }
 
         private static void showCurrentDate()
@@ -78,9 +110,10 @@ namespace Ex04.Menus.Test
 
             Console.WriteLine("Please enter a sentence:");
             userInput = Console.ReadLine();
-            foreach(char currentChar in userInput)
+
+            foreach (char currentChar in userInput)
             {
-                if(char.IsUpper(currentChar))
+                if (char.IsUpper(currentChar))
                 {
                     upperCaseCount++;
                 }
